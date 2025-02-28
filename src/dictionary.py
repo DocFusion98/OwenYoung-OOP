bc779ba2f45fa14ca4bf071afab31727337892d2
DocFusion98 <youngowen@gmail.com>
2025-02-28 00:33:46 -0500

    templates lab

--- src/dictionary.py
+++ src/dictionary.py
@@ -1,1 +1,4 @@
-d = ["/images/ClownPeter.png", "/images/Devin.png", "/images/Blu.png"]
+pfps = ["/images/ClownPeter.png", "/images/Devin.png", "/images/Blu.png"]
+
+
+names = ["Devin", "Hudson", "Owen", "Doc", "Freddy Fazbear", "Fatt", "Swollin", "Ronaldo", "Jacky", "X Æ A-12"]
--- src/main.py
+++ src/main.py
@@ -10,7 +10,9 @@ class App:
     @cherrypy.expose
     def index(self):
         t = mako.template.Template(filename=f"{PYPATH}/../html/home.html")
-        return t.render()
+        n = random.randint(0, len(dictionary.names) - 1)
+        name = dictionary.names[n]
+        return t.render(NAME = name)
 
     @cherrypy.expose
     def signup(self):
@@ -23,8 +25,15 @@ class App:
         d = random.randint(0, 365)
         v = random.randint(1, 10000420)
         i = random.randint(0, 2)
-        i = dictionary.d[i]
-        return t.render(DAYS = d, VIEWS = v, PFP = i)
+        p1 = dictionary.pfps[i]
+        i = random.randint(0, len(dictionary.pfps) - 1)
+        p2 = dictionary.pfps[i]
+        while p1 == p2:
+            i = random.randint(0, 2)
+            p2 = dictionary.pfps[i]
+        
+        
+        return t.render(DAYS = d, VIEWS = v, PFP1 = p1, PFP2 = p2)
 
 app = App()
 cherrypy.quickstart(app, "/",
